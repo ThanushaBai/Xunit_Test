6a1442af2371da17565151c660870f2ea2ee1e95
ThanushaBai <thanushabai12@gmail.com>
2025-12-15 20:12:50 +0530

    Initial commit with Calculator and tests

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(1, calc.subtract(3, 2));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(6, calc.multiply(2, 3));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calc.divide(6, 3));
+    }
+
+    @Test
+    void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calc.divide(5, 0));
+    }
+}
